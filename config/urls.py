4bab3bb220ff683d1e5b7d3c6ae8dfb47a757f60
Turdiali <xasanbayevturdiali7@gmail.com>
2026-08-25 20:51:36 +0500

    bug fix

--- config/urls.py
+++ config/urls.py
@@ -44,5 +44,5 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    urlpatterns += [re_path(r'^/static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})]
-    urlpatterns += [re_path(r'^/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+    urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})]
+    urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
